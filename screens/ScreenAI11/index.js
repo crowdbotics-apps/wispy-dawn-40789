a1d5d5a5bf6b0cd7e2fa6db219030f005aa2ec5b
kadir diaz <workforut@gmail.com>
2023-05-17 18:33:40 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/ScreenAI11/index.js
+++ screens/ScreenAI11/index.js
@@ -1,48 +1,52 @@
 import { Pressable } from "react-native";
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
 const ScreenTwo = () => {
   return <View style={styles.container}>
       <View style={styles.header}>
-        <Pressable><Text style={styles.headerText}>Tasks</Text></Pressable>
+        <Pressable>
+          <Text style={styles.headerText}>Tasks</Text>
+        </Pressable>
       </View>
       <View style={styles.taskContainer}>
         <TouchableOpacity style={styles.taskButton}>
-          <Pressable><Image source={{
-            uri: 'https://tinyurl.com/42evm3m3'
-          }} style={styles.taskImage} /></Pressable>
-          <Text style={styles.taskText}>Task 1</Text>
+          <Pressable>
+            <Image source={{
+            uri: "https://tinyurl.com/42evm3m3"
+          }} style={styles.taskImage} />
+          </Pressable>
+          <Text style={styles.taskText}>Schedule</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.taskButton}>
           <Image source={{
-          uri: 'https://tinyurl.com/42evm3m3'
+          uri: "https://tinyurl.com/42evm3m3"
         }} style={styles.taskImage} />
-          <Text style={styles.taskText}>Task 2</Text>
+          <Text style={styles.taskText}>Fire Fly</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.taskButton}>
           <Image source={{
-          uri: 'https://tinyurl.com/42evm3m3'
+          uri: "https://tinyurl.com/42evm3m3"
         }} style={styles.taskImage} />
-          <Text style={styles.taskText}>Task 3</Text>
+          <Text style={styles.taskText}>Pallets</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.taskButton}>
           <Image source={{
-          uri: 'https://tinyurl.com/42evm3m3'
+          uri: "https://tinyurl.com/42evm3m3"
         }} style={styles.taskImage} />
-          <Text style={styles.taskText}>Task 4</Text>
+          <Text style={styles.taskText}>Rolls</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.taskButton}>
           <Image source={{
-          uri: 'https://tinyurl.com/42evm3m3'
+          uri: "https://tinyurl.com/42evm3m3"
         }} style={styles.taskImage} />
-          <Text style={styles.taskText}>Task 5</Text>
+          <Text style={styles.taskText}>Issues</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.taskButton}>
           <Image source={{
-          uri: 'https://tinyurl.com/42evm3m3'
+          uri: "https://tinyurl.com/42evm3m3"
         }} style={styles.taskImage} />
-          <Text style={styles.taskText}>Task 6</Text>
+          <Text style={styles.taskText}>Tracking</Text>
         </TouchableOpacity>
       </View>
     </View>;
@@ -51,34 +55,34 @@ const ScreenTwo = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: "#fff"
   },
   header: {
     height: 80,
-    backgroundColor: '#2c3e50',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: "#2c3e50",
+    justifyContent: "center",
+    alignItems: "center"
   },
   headerText: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 24,
-    fontWeight: 'bold'
+    fontWeight: "bold"
   },
   taskContainer: {
     flex: 1,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-evenly",
+    alignItems: "center",
     padding: 20
   },
   taskButton: {
-    width: '40%',
+    width: "40%",
     height: 150,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1",
     borderRadius: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     marginVertical: 10
   },
   taskImage: {
@@ -88,8 +92,8 @@ const styles = StyleSheet.create({
   },
   taskText: {
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center'
+    fontWeight: "bold",
+    textAlign: "center"
   }
 });
 export default ScreenTwo;
